19c218c2d66e468a5daa611f0607f8b6f7b4038f
Francisco Marmol <20240348@ricaldone.edu.sv>
2025-07-13 19:22:54 -0600

    trabaje tbWorkOrders y tbVehicleEntries

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Entities/VehiclesEntrys/VehicleEntryEntity.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/Entities/VehiclesEntrys/VehicleEntryEntity.java
@@ -1,4 +1,38 @@
 package DevSGMA_PTC.SGMA_PTC.Entities.VehiclesEntrys;
 
+//Importacion de librerias
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbVehicleEntries")
+@Data //No es necesario colocar @To String, Ya que el Data lo sustituye
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleEntryEntity {
-}
+
+    //creamos y ponemos los atributos haciendo relacion a la base de datos del Proyecto
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String licensePlate;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private LocalDateTime entryDate;
+
+    @Column
+    private String notes;
+
+    }
+
