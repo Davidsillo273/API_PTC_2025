85504738949a0fd9af8991364d9650707011cf96
David <20240273@ricaldone.edu.sv>
2025-07-14 23:37:55 -0600

    Persona 1 mejorado

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Entities/Users/UserEntity.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/Entities/Users/UserEntity.java
@@ -1,37 +1,59 @@
 package DevSGMA_PTC.SGMA_PTC.Entities.Users;
 
+import DevSGMA_PTC.SGMA_PTC.Entities.Roles.RolEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
-import javax.management.relation.Role;
+import lombok.*;
 
+@Getter @Setter
 public class UserEntity {
 
-    // Esta clase representa la tabla tbUsers de la base de datos
-    @Entity
-    @Table(name = "tbUsers")
-    @Data // Lombok: genera automáticamente getters, setters, equals, hashCode y toString
-    @NoArgsConstructor // Lombok: constructor vacío
-    @AllArgsConstructor // Lombok: constructor con todos los campos
+    //Atributos
+    @Column(name = "userId")
+    private Long userId;
 
-    public class User {
+    @Column(name = "email")
+    private String email;
 
-        @Id // Llave primaria
-        @GeneratedValue(strategy = GenerationType.IDENTITY) // Se genera automáticamente (autoincremental)
-        private Long userId;
+    @Column(name = "name")
+    private String username;
 
-        @Column(nullable = false, unique = true)
-        private String username;
+    @Column(name = "passwordHash")
+    private String passwordHash;
 
-        @Column(nullable = false)
-        private String passwordHash;
+    @Column(name = "fullName")
+    private String fullName;
 
-        @Column(nullable = false)
-        private String fullName;
+    @ManyToOne
+    @JoinColumn(name = "roleId", referencedColumnName = "roleId")
+    private RolEntity role;
 
-        @ManyToOne // Muchos usuarios pueden tener un mismo rol
-        @JoinColumn(name = "roleId", nullable = false) // Columna que conecta con la tabla de roles
-        private Role role;}
 }
+
+//    // Esta clase representa la tabla tbUsers de la base de datos
+//    @Entity
+//    @Table(name = "tbUsers")
+//    @Data // Lombok: genera automáticamente getters, setters, equals, hashCode y toString
+//    @NoArgsConstructor // Lombok: constructor vacío
+//    @AllArgsConstructor // Lombok: constructor con todos los campos
+//
+//    public class User {
+//
+//        @Id // Llave primaria
+//        @GeneratedValue(strategy = GenerationType.IDENTITY) // Se genera automáticamente (autoincremental)
+//        private Long userId;
+//
+//        @Column(nullable = false, unique = true)
+//        private String username;
+//
+//        @Column(nullable = false)
+//        private String passwordHash;
+//
+//        @Column(nullable = false)
+//        private String fullName;
+//
+//        @ManyToOne // Muchos usuarios pueden tener un mismo rol
+//        @JoinColumn(name = "roleId", nullable = false) // Columna que conecta con la tabla de roles
+//        private Role role;
+//    }
--- src/main/java/DevSGMA_PTC/SGMA_PTC/Services/Users/UserService.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/Services/Users/UserService.java
@@ -1,5 +1,7 @@
 package DevSGMA_PTC.SGMA_PTC.Services.Users;
 
+import DevSGMA_PTC.SGMA_PTC.Entities.Users.UserEntity;
+import DevSGMA_PTC.SGMA_PTC.Models.DTO.Users.UserRequestDTO;
 import DevSGMA_PTC.SGMA_PTC.Repositories.Users.UserRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,30 +9,46 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 // Capa de lógica de negocio. Aquí validamos antes de guardar o consultar datos
-@Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository; // Repositorio que accede a la base de datos
 
     // Devuelve todos los usuarios
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserRequestDTO> getAllUsers() {
+        List<UserEntity> User = userRepository.findAll();
+        return User.stream()
+                .map(this::convertToUserDTO)
+                .collect(Collectors.toList());
     }
 
-    // Busca un usuario por su ID
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
+    public UserRequestDTO convertToUserDTO(UserEntity entity){
 
-    // Guarda o actualiza un usuario
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
-    }
-
-    // Elimina un usuario por ID
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        //Este será el obj a retornar
+        UserRequestDTO dto = new UserRequestDTO();
+        dto.setUserId(entity.getUserId());
+        dto.setEmail(entity.getEmail());
+        dto.setUsername(entity.getUsername());
+        dto.setFullName(entity.getFullName());
+        dto.setPasswordHash(entity.getPasswordHash());
+        return dto;
     }
 }
+
+//    // Busca un usuario por su ID
+//    public User saveUser(User user) {
+//        return userRepository.save(user);
+//    }
+//
+//    // Guarda o actualiza un usuario
+//    public Optional<User> getUserById(Long id) {
+//        return userRepository.findById(id);
+//    }
+//
+//    // Elimina un usuario por ID
+//    public void deleteUser(Long id) {
+//        userRepository.deleteById(id);
+//    }
