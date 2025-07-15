a0d5a3471979b3fc57839083d920357603ffe328
Francisco Marmol <20240348@ricaldone.edu.sv>
2025-07-14 23:48:40 -0600

    TBWorkOrders Y TBVehicleEntries finalizazdo

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Models/DTO/VehiclesEntrys/VehicleEntryRequestDTO.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/Models/DTO/VehiclesEntrys/VehicleEntryRequestDTO.java
@@ -1,12 +1,19 @@
 package DevSGMA_PTC.SGMA_PTC.Models.DTO.VehiclesEntrys;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Entity
+
+@ToString @EqualsAndHashCode
+@Getter @Setter
 @Table(name = "tbVehicleEntries")
 public class VehicleEntryRequestDTO {
-    private String licensePlate; // No necesita @Column
+
+    private String licensePlate;
     private String brand;
     private String model;
     private String notes;
--- src/main/java/DevSGMA_PTC/SGMA_PTC/Repositories/WorksOrders/WorkOrderRepository.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/Repositories/WorksOrders/WorkOrderRepository.java
@@ -1,13 +1,16 @@
 package DevSGMA_PTC.SGMA_PTC.Repositories.WorksOrders;
 
-import DevSGMA_PTC.SGMA_PTC.Models.Entities.WorkOrder;
+import DevSGMA_PTC.SGMA_PTC.Entities.WorksOrders.WorkOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface WorkOrderRepository extends JpaRepository<WorkOrder, Long> {
-    List<WorkOrder> findByStatus(WorkOrder.WorkOrderStatus status);
-    List<WorkOrder> findByAcademicYearAndStatus(WorkOrder.AcademicYear academicYear, WorkOrder.WorkOrderStatus status);
+public interface WorkOrderRepository extends JpaRepository<WorkOrderEntity, Long> {
+    Optional<WorkOrderEntity> findByStatus(String findByStatus);
+
+//    List<> findByStatus(WorkOrder.WorkOrderStatus status);
+//    List<WorkOrder> findByAcademicYearAndStatus(WorkOrder.AcademicYear academicYear, WorkOrder.WorkOrderStatus status);
 }
