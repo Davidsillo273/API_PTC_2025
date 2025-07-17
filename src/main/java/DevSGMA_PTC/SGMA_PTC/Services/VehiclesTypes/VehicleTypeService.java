d2a76d27610c73dcbb838cd3b18cdeae00093e38
TylerHui <anthonytyler.hui.7@gmail.com>
2025-07-16 18:26:38 -0600

    Vehicles y VehiclesTypes

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Services/VehiclesTypes/VehicleTypeService.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/Services/VehiclesTypes/VehicleTypeService.java
@@ -1,4 +1,42 @@
-package DevSGMA_PTC.SGMA_PTC.Services.VehiclesTypes;
+package DevSGMA_PTC.SGMA_PTC.Service;
+
+import DevSGMA_PTC.SGMA_PTC.Entities.vehicleTypeEntity;
+import DevSGMA_PTC.SGMA_PTC.Models.DTO.VehiclesTypes.VehicleTypeResponseDTO;
+import DevSGMA_PTC.SGMA_PTC.Repositories.VehiclesTypes.VehicleTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 
 public class VehicleTypeService {
+
+    @Autowired
+    private VehicleTypeRepository repo;
+
+    public List<VehicleTypeResponseDTO> obtenerTodo() {
+
+        List<vehicleTypeEntity>VehicleTypes = repo.findAll();
+        return VehicleTypes.stream()
+                .map(this::convertirVehicleTypesDTO)
+                .collect(Collectors.toList());
+    }
+
+    public VehicleTypeResponseDTO convertirVehicleTypesDTO(vehicleTypeEntity entity){
+
+        VehicleTypeResponseDTO dto = new VehicleTypeResponseDTO();
+        dto.setTypeId(entity.getTypeId());
+        dto.setTypeName(entity.getTypeName());
+        return dto;
+    }
+
+    
 }
+
+
+
+
+
