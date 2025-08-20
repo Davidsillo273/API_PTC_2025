7a9b81d73e8cd50f234efa578228ffefa3550d29
David <20240273@ricaldone.edu.sv>
2025-08-20 17:46:01 -0600

    Se está eliminando código inecesario (Roles)

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Repositories/Roles/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package DevSGMA_PTC.SGMA_PTC.Repositories.Roles;
-
-import DevSGMA_PTC.SGMA_PTC.Entities.Roles.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-}
