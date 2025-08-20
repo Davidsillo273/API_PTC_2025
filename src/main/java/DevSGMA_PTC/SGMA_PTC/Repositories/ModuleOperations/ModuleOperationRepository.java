21dff1b5caa3ec778fdf5372b31206e087d3420b
David <20240273@ricaldone.edu.sv>
2025-08-20 17:52:23 -0600

    Se eliminó demás código (Modules y ModulesOperations)

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Repositories/ModuleOperations/ModuleOperationRepository.java
+++
@@ -1,8 +0,0 @@
-package DevSGMA_PTC.SGMA_PTC.Repositories.ModuleOperations;
-
-import DevSGMA_PTC.SGMA_PTC.Entities.ModuleOperations.ModuleOperation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModuleOperationRepository extends JpaRepository<ModuleOperation, Long> {
-    boolean existsByNameAndModuleId(String name, Long moduleId);
-}
--- src/main/java/DevSGMA_PTC/SGMA_PTC/Repositories/Modules/ModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package DevSGMA_PTC.SGMA_PTC.Repositories.Modules;
-
-import DevSGMA_PTC.SGMA_PTC.Entities.Modules.Module;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModuleRepository extends JpaRepository<Module, Long> {
-}
