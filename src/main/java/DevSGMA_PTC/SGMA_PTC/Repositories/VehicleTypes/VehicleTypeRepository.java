1b9934a6ab7b0544bcab760808aa975c3f0584bf
David <20240273@ricaldone.edu.sv>
2025-08-20 18:44:14 -0600

    Se eliminó TODO

--- src/main/java/DevSGMA_PTC/SGMA_PTC/Repositories/VehicleTypes/VehicleTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package DevSGMA_PTC.SGMA_PTC.Repositories.VehicleTypes;
-
-import DevSGMA_PTC.SGMA_PTC.Entities.VehicleTypes.VehicleType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehicleTypeRepository extends JpaRepository<VehicleType, Long> {
-}
