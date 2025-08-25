d642a3d46247aa8fd1915ae38ed727d4996a47c1
David <20240273@ricaldone.edu.sv>
2025-08-25 11:51:22 -0600

    Se agregó Cloudinary y se comentó cosas del POM, application.properties y se modificó en la Application

--- src/main/java/DevSGMA_PTC/SGMA_PTC/SgmaPtcApplication.java
+++ src/main/java/DevSGMA_PTC/SGMA_PTC/SgmaPtcApplication.java
@@ -7,13 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SgmaPtcApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
+    public static void main(String[] args) {
 
-		SpringApplication.run(SgmaPtcApplication.class, args);
-	}
+        loadEnvVariables();
+
+        SpringApplication.run(SgmaPtcApplication.class, args);
+    }
+
+    static void loadEnvVariables() {
+        // Carga las variables de entorno desde un archivo .env usando la librería Dot
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        // Establece cada variable de entorno como una propiedad del sistema
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+
+    }
 
 }
